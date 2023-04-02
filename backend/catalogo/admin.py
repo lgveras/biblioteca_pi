369a55b8099deeb0a0ed5f43167db839cff078fe
edulonde <edulonde@gmail.com>
2023-04-01 21:00:11 -0300

    criação do app catalogo

---
+++ backend/catalogo/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+from .models import Status, Editora, Livro
+
+admin.site.register(Status)
+admin.site.register(Editora)
+admin.site.register(Livro)
---
+++ backend/catalogo/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+
+class Status(models.Model):
+    descricao_status = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.descricao_status
+
+
+class Editora(models.Model):
+    nome_editora = models.CharField(max_length=200)
+    local_editora = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nome_editora
+
+
+class Livro(models.Model):
+    titulo = models.CharField(max_length=200)
+    subtitulo = models.CharField(max_length=400, blank=True)
+    autor = models.CharField(max_length=200)
+    isbn = models.CharField(max_length=20, blank=True)
+    ano = models.IntegerField(blank=True, null=True)
+    editora = models.ForeignKey(Editora, on_delete=models.CASCADE)
+    assunto = models.CharField(max_length=200)
+    # status = models.ForeignKey(Status, on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return self.titulo
